d048674cc4b50ece9a54c69f1053bddd54ab6257
John Isaac Calderon <ogclapdrop@gmail.com>
2025-10-19 20:44:19 +0200

    Implemented part 1
    This implementation has yet to be audited, but it should do.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

--- part1.s
+++ part1.s
@@ -4,16 +4,24 @@
 @ Main function of program
 main:
     @ First load the desired Fibonacci number
-    mov r0, #13 @ N
+    mov r2, #13 @ N
+
     @ Initialize the first two numbers in the sequence
-    mov r1, #0  @ current
-    mov r2, #1  @ previous
+    mov r0, #0  @ current
+    mov r1, #1  @ previous
 
 @ Loop will do the main work calculating the Fibonacci sequence
-loop:
-    @ ! Implement your logic here !
-    @ Jump to start off loop
-    b loop
+@ - uses R2 to count the number of iterations
+@ - final result is in R0
+.top:
+    mov r3, r0          @ move current value to temporary register
+    add r0, r0, r1      @ R0 = R0 + R1
+    mov r1, r3          @ set previous value to old current value
+    
+    @ Decrement counter and continue if R2 != 0
+    subs r2, r2, #1
+    bnz .top
+    @ -- fall-through on exhaustion -- @
 
 @ To be a good citizen we branch (and exchange) on the lr register
 @ to return to the caller
